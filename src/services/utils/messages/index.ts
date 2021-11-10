0a7288cadcdfeb81ae4c5f2e0fc7e596b33c0f45
AnthonyLzq <sluzquinosa@uni.pe>
2021-11-10 18:08:25 -0500

    feat: updated error message to include contact mail

--- src/services/utils/messages/index.ts
+++ src/services/utils/messages/index.ts
@@ -1,5 +1,5 @@
 enum GenericErrors {
-  INTERNAL_SERVER_ERROR = 'Something went wrong'
+  INTERNAL_SERVER_ERROR = 'Something went wrong, please contact with anthony.luzquinos@gmail.com'
 }
 
 export { EFC, MFC } from './csv'
